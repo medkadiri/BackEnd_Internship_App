f6298b423a1a194f229eae2733d2482dcaf2fcf3
Mohamed El Kadiri El Hassani <m.elkadirielhassani@aui.ma>
2022-03-18 16:31:58 +0100

    failing test

--- src/todo/todo.controller.spec.ts
+++ src/todo/todo.controller.spec.ts
@@ -1,18 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoController } from './todo.controller';
+import { TodoEntity } from './todo.entity';
+import { TodoService } from './todo.service';
 
 describe('TodoController', () => {
   let controller: TodoController;
+  let todoService: TodoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TodoController],
+      providers: [TodoService],
     }).compile();
 
     controller = module.get<TodoController>(TodoController);
+    todoService = module.get<TodoService>(TodoService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('listAll', () => {
+    it('should return an list of todo', async () => {
+      var result: TodoEntity[];
+      jest.spyOn(todoService, 'listAll').mockResolvedValue(result);
+
+      expect(await controller.listAll()).toBe(result);
+    });
   });
 });
