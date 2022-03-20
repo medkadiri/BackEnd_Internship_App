4bb677398376e82ffde369c48a6deebd90c766af
Mohamed El Kadiri El Hassani <m.elkadirielhassani@aui.ma>
2022-03-20 16:58:34 +0100

    added test for todo.controller - passed

--- src/todo/todo.controller.spec.ts
+++ src/todo/todo.controller.spec.ts
@@ -1,32 +1,42 @@
+import { HttpStatus } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoController } from './todo.controller';
-import { TodoEntity } from './todo.entity';
+import { TodoDTO } from './todo.dto';
 import { TodoService } from './todo.service';
 
 describe('TodoController', () => {
   let controller: TodoController;
-  let todoService: TodoService;
+
+  const mockTodoService = {
+    add: jest.fn(dto => {
+      return dto;
+    })
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TodoController],
       providers: [TodoService],
-    }).compile();
+    })
+      .overrideProvider(TodoService)
+      .useValue(mockTodoService)
+      .compile();
 
     controller = module.get<TodoController>(TodoController);
-    todoService = module.get<TodoService>(TodoService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
-  describe('listAll', () => {
-    it('should return an list of todo', async () => {
-      var result: TodoEntity[];
-      jest.spyOn(todoService, 'listAll').mockResolvedValue(result);
-
-      expect(await controller.listAll()).toBe(result);
-    });
+  it('should add todo', async () => {
+    const dto = { id: 1, description: 'Testing' };
+    const returned = {
+      statusCode: HttpStatus.OK,
+      message: 'Todo created successfully',
+      todo: dto
+    };
+    expect(await controller.createTodo(dto)).toEqual(returned);
   });
+
 });
--- src/todo/todo.service.spec.ts
+++ src/todo/todo.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { TodoEntity } from './todo.entity';
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
   let service: TodoService;
 
+  const mockTodoRepository = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoService],
+      // imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature([TodoEntity])],
+      providers: [TodoService, {
+        provide: getRepositoryToken(TodoEntity),
+        useValue: mockTodoRepository,
+      }],
     }).compile();
 
     service = module.get<TodoService>(TodoService);
